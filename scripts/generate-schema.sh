1bbfeb9286579228d7449707bf1bade037fdc0a9
david-waltermire-nist <david.waltermire@nist.gov>
2020-01-23 08:32:57 -0500

    added configuration support to build scripts

--- scripts/generate-schema.sh
+++ scripts/generate-schema.sh
@@ -4,27 +4,25 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source "${script_dir}/include/common-environment.sh"
 
 # Option defaults
-WORKING_DIR="${PWD}"
-CACHE_DIR="${WORKING_DIR}/.metaschema-cache"
 GENERATE_SCHEMA="json"
-PROVIDER_DIR="${script_dir}/../toolchains/oscal-m2"
-VERBOSE=false
-HELP=false
+VALIDATE_SCHEMA=false
 
 usage() {                                      # Function: Print a help message.
   cat << EOF
 Usage: $0 [options] metaschema_file [generated_schema_file]
 
 -h, --help                        Display help
--w DIR, --working-dir DIR         Generate artifacts in DIR
---cache-dir dir                   Store used artifacts in DIR (e.g., schema for validation)
+-w DIR, --working-dir DIR         Generate artifacts in DIR (default: ${WORKING_DIR})
+--cache-dir DIR                   Store used artifacts in DIR (e.g., schema for validation) (default: ${CACHE_DIR})
+--provider-dir DIR                The Metaschema framework provider to use (default: ${PROVIDER_DIR})
+--validate-schema                 Perform schema validation after generating the schema
 -v                                Provide verbose output
 --xml                             Generate an XML schema
 --json                            Generate a JSON schema (default)
 EOF
 }
 
-OPTS=`getopt -o w:vh --long working-dir:,help,xml,json -n "$0" -- "$@"`
+OPTS=`getopt -o w:vh --long working-dir:,cache-dir:,provider-dir:,validate,help,xml,json -n "$0" -- "$@"`
 if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; usage ; exit 1 ; fi
 
 # Process arguments
@@ -40,11 +38,17 @@ while [ $# -gt 0 ]; do
       CACHE_DIR="$(realpath "$2")"
       shift # past path
       ;;
+    --provider-dir)
+      PROVIDER_DIR="$(realpath "$2")"
+      ;;
     --xml)
       GENERATE_SCHEMA="xml"
       ;;
     --json)
       GENERATE_SCHEMA="json"
+      ;;
+    --validate)
+      VALIDATE_SCHEMA=true
       ;;
     -v)
       VERBOSE=true
@@ -70,9 +74,15 @@ OTHER_ARGS=$@ # save the remaining args
 METASCHEMA="$1"
 GENERATED_SCHEMA="$2"
 
-if [ ! -f "$METASCHEMA" ]; then
-  echo -e "${P_ERROR}Metaschema does not exist:${P_END} ${METASCHEMA}"
+if [ -z "$METASCHEMA" ]; then
+  >&2 echo -e "${P_ERROR}You must specify the Metaschema to generate the schema for.${P_END}"
+  usage
   exit 1
+fi
+
+if [ ! -f "$METASCHEMA" ]; then
+  >&2 echo -e "${P_ERROR}Metaschema does not exist:${P_END} ${METASCHEMA}"
+  exit 2
 fi
 
 # initialize provider
@@ -96,7 +106,7 @@ fi
 # make sure working directory exists
 mkdir -p "$(dirname "$WORKING_DIR")"
 
-METASCHEMA_RELATIVE_PATH=$(realpath --relative-to="${WORKING_DIR}" "$METASCHEMA")
+METASCHEMA_RELATIVE_PATH=$(get_rel_path "${WORKING_DIR}" "${METASCHEMA}")
 
 if [ "$VERBOSE" = "true" ]; then
   echo -e "${P_INFO}Generating ${GENERATE_SCHEMA^^} schema for metaschema:${P_END} ${METASCHEMA_RELATIVE_PATH}"
@@ -110,3 +120,34 @@ if [ $cmd_exitcode -eq 0 ]; then
   fi
   echo -e "${result}"
 fi
+
+if [ "$VALIDATE_SCHEMA" == "true" ]; then
+  # validate generated schema
+  case ${GENERATE_SCHEMA} in
+  xml)
+    result=$(xmllint --noout --schema "$OSCALDIR/build/ci-cd/support/XMLSchema.xsd" "$GENERATED_SCHEMA" 2>&1)
+    cmd_exitcode=$?
+    ;;
+  json)
+    result=$(validate_json "$OSCALDIR/build/ci-cd/support/json-schema-schema.json" "$GENERATED_SCHEMA")
+    cmd_exitcode=$?
+    ;;
+  *)
+    echo -e "${P_WARN}${GENERATE_SCHEMA^^} schema validation not supported for '${P_END}${GENERATED_SCHEMA}${P_WARN}'.${P_END}"
+    cmd_exitcode=0
+    ;;
+  esac
+
+  if [ $cmd_exitcode -ne 0 ]; then
+    echo -e "${P_ERROR}Schema validation failed for '${P_END}${GENERATED_SCHEMA}${P_ERROR}'.${P_END}"
+    echo -e "${P_ERROR}${result}${P_END}"
+    exitcode=1
+  else
+    if [ "$VERBOSE" = "true" ]; then
+      echo -e "${P_OK}Schema validation passed for '${P_END}${GENERATED_SCHEMA}${P_OK}'.${P_END}"
+    else
+      echo -e "${P_OK}Schema generation passed for '${P_END}${METASCHEMA}${P_OK}' as '${P_END}${GENERATED_SCHEMA}${P_OK}', which is valid.${P_END}"
+    fi
+  fi
+fi
+
--- scripts/include/common-environment.sh
+++ scripts/include/common-environment.sh
@@ -1,37 +1,128 @@
+#!/bin/bash
 
-## Setup color codes
-# check if stdout is a terminal...
-export TERM=${TERM:-dumb}
-colorize=0
-if [ -t 1 ]; then
-  # does the terminal support colors?
-  ncolors=$(tput colors)
-  if [ -n "$ncolors" ] && [ $ncolors -ge 8 ]; then
-    colorize=1
+if [ -z ${METASCHEMA_SCRIPT_INIT+x} ]; then
+
+  function_exists() {
+      declare -f -F $1 > /dev/null
+      return $?
+  }
+
+  get_abs_path() {
+    local path="$1"
+    local abs_path
+
+    if [ "$path" == "." ]; then
+      abs_path="$(pwd)"
+    elif [ "$path" == ".." ]; then
+      abs_path="$(dirname "$(pwd)")"
+    else
+      abs_path="$(cd "$(dirname "$path")"; pwd)/$(basename "$path")"
+    fi
+    echo "$abs_path"
+  }
+
+  get_rel_path() {
+    # modified from https://stackoverflow.com/questions/2564634/convert-absolute-path-into-relative-path-given-a-current-directory-using-bash/12498485#12498485
+
+    # the source and target are expected to be absolute paths
+    local source=$(get_abs_path "$1")
+    local target=$(get_abs_path "$2")
+
+    local common_part="$source" # star with source for now
+    local result="" # empty for now
+
+    while [[ "${target#$common_part}" == "${target}" ]]; do
+      # no match, means that candidate common part is not correct
+      # go up one level (reduce common part)
+      common_part="$(dirname $common_part)"
+      # and record that we went back, with correct / handling
+      if [[ -z $result ]]; then
+          result=".."
+      else
+          result="../$result"
+      fi
+    done
+
+    if [[ $common_part == "/" ]]; then
+        # special case for root (no common path)
+        result="$result/"
+    fi
+
+    # since we now have identified the common part,
+    # compute the non-common part
+    local forward_part="${target#$common_part}"
+
+    # and now stick all parts together
+    if [[ -n $result ]] && [[ -n $forward_part ]]; then
+        result="$result$forward_part"
+    elif [[ -n $forward_part ]]; then
+        # extra slash removal
+        result="${forward_part:1}"
+    fi
+
+    echo $result  
+  }
+
+  if [ -z ${METASCHEMA_SCRIPT_DIR+x} ]; then
+    METASCHEMA_SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+    METASCHEMA_SCRIPT_DIR="$(cd "${METASCHEMA_SCRIPT_DIR}/.."; pwd)"
   fi
-fi
 
-if [ $colorize -eq 1 ]; then
-    #setup print colors
-#    P_ERROR="$(tput setaf 1)$(tput bold)"
-#    P_OK="$(tput setaf 2)$(tput bold)"
-#    P_WARN="$(tput setaf 3)$(tput bold)"
-#    P_INFO="$(tput bold)"
-#    P_END="$(tput sgr0)"
-    P_ERROR="\u001b[31m\u001b[1m"
-    P_OK="\u001b[32m\u001b[1m"
-    P_WARN="\u001b[33m\u001b[1m"
-    P_INFO="\u001b[37m\u001b[1m" # white bold
-    P_END="\u001b[0m" # reset
-else
-    P_ERROR=""
-    P_OK=""
-    P_WARN=""
-    P_INFO=""
-    P_END=""
-fi
+  if [ -f "${METASCHEMA_SCRIPT_DIR}/config.sh" ]; then
+    source "${METASCHEMA_SCRIPT_DIR}/config.sh"
+  fi
 
-function_exists() {
-    declare -f -F $1 > /dev/null
-    return $?
-}
+  # set meaningful defaults if not set above
+  if [ -z ${PROVIDER_DIR+x} ]; then
+    PROVIDER_DIR="${METASCHEMA_SCRIPT_DIR}/../toolchains/oscal-m2"
+  fi
+
+  if [ -z ${WORKING_DIR+x} ]; then
+    WORKING_DIR="${PWD}"
+  fi
+    
+  if [ -z ${CACHE_DIR+x} ]; then
+    CACHE_DIR="${WORKING_DIR}/.metaschema-cache"
+  fi
+
+  if [ -z ${VERBOSE+x} ]; then
+    VERBOSE=true
+  fi
+
+  PROVIDER_DIR=$(get_abs_path "${PROVIDER_DIR}")
+  WORKING_DIR=$(get_abs_path "${WORKING_DIR}")
+  CACHE_DIR=$(get_abs_path "${CACHE_DIR}")
+
+  # Make directories
+  mkdir -p "${WORKING_DIR}"
+  mkdir -p "${CACHE_DIR}"
+
+  ## Setup color codes
+  # check if stdout is a terminal...
+  export TERM=${TERM:-dumb}
+  colorize=0
+  if [ -t 1 ]; then
+    # does the terminal support colors?
+    ncolors=$(tput colors)
+    if [ -n "$ncolors" ] && [ $ncolors -ge 8 ]; then
+      colorize=1
+    fi
+  fi
+
+  if [ $colorize -eq 1 ]; then
+      #setup print colors
+      P_ERROR="\u001b[31m\u001b[1m"
+      P_OK="\u001b[32m\u001b[1m"
+      P_WARN="\u001b[33m\u001b[1m"
+      P_INFO="\u001b[37m\u001b[1m" # white bold
+      P_END="\u001b[0m" # reset
+  else
+      P_ERROR=""
+      P_OK=""
+      P_WARN=""
+      P_INFO=""
+      P_END=""
+  fi
+
+  METASCHEMA_SCRIPT_INIT=true
+fi
