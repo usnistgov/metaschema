6f96ede94cf385a6d41c5186dab10e4d8a8bf120
david-waltermire-nist <david.waltermire@nist.gov>
2020-01-27 21:59:07 -0500

    Implemented content converter generation in build scripts

--- scripts/include/common-environment.sh
+++ scripts/include/common-environment.sh
@@ -28,13 +28,13 @@ if [ -z ${METASCHEMA_SCRIPT_INIT+x} ]; then
     local source=$(get_abs_path "$1")
     local target=$(get_abs_path "$2")
 
-    local common_part="$source" # star with source for now
+    local common_part="$source" # start with source for now
     local result="" # empty for now
 
-    while [[ "${target#$common_part}" == "${target}" ]]; do
+    while [ "${target#$common_part}" == "${target}" ]; do
       # no match, means that candidate common part is not correct
       # go up one level (reduce common part)
-      common_part="$(dirname $common_part)"
+      common_part="$(dirname "$common_part")"
       # and record that we went back, with correct / handling
       if [[ -z $result ]]; then
           result=".."
@@ -43,7 +43,7 @@ if [ -z ${METASCHEMA_SCRIPT_INIT+x} ]; then
       fi
     done
 
-    if [[ $common_part == "/" ]]; then
+    if [ $common_part == "/" ]; then
         # special case for root (no common path)
         result="$result/"
     fi
@@ -55,12 +55,70 @@ if [ -z ${METASCHEMA_SCRIPT_INIT+x} ]; then
     # and now stick all parts together
     if [[ -n $result ]] && [[ -n $forward_part ]]; then
         result="$result$forward_part"
-    elif [[ -n $forward_part ]]; then
+    elif [ -n $forward_part ]; then
         # extra slash removal
         result="${forward_part:1}"
     fi
 
     echo $result  
+  }
+
+  initialize_defaults() {
+    # set meaningful defaults if not set by local configuration
+    if [ -z ${PROVIDER_DIR+x} ]; then
+      PROVIDER_DIR="${METASCHEMA_SCRIPT_DIR}/../toolchains/oscal-m2"
+    fi
+
+    if [ -z ${WORKING_DIR+x} ]; then
+      WORKING_DIR="${PWD}"
+    fi
+      
+    if [ -z ${CACHE_DIR+x} ]; then
+      CACHE_DIR="${WORKING_DIR}/.metaschema-cache"
+    fi
+
+    if [ -z ${VERBOSE+x} ]; then
+      VERBOSE=false
+    fi
+
+    PROVIDER_DIR=$(get_abs_path "${PROVIDER_DIR}")
+    WORKING_DIR=$(get_abs_path "${WORKING_DIR}")
+    CACHE_DIR=$(get_abs_path "${CACHE_DIR}")
+  }
+
+  init_provider() {
+    # initialize provider
+    PROVIDER_PATH="$PROVIDER_DIR"
+    if [ "$VERBOSE" = "true" ]; then
+      echo -e "${P_INFO}Using Metaschema provider:${P_END} ${PROVIDER_PATH}"
+    fi
+    PROVIDER_INIT_FILE="${PROVIDER_PATH}/init.sh"
+    source "${PROVIDER_INIT_FILE}"
+
+    toolchain_provider_init
+    cmd_exitcode=$?
+    if [ $cmd_exitcode -ne 0 ]; then
+      >&2 echo -e "${P_ERROR}Failed to execute the initialization function for Metaschema provider '${P_END}${PROVIDER_DIR}${P_ERROR}'.${P_END}"
+      exit 1
+    fi
+
+    if ! function_exists "generate_xml_schema"; then
+      >&2 echo -e "${P_ERROR}The function '${P_END}generate_xml_schema${P_ERROR}' isn't defined in:${P_END} ${PROVIDER_INIT_FILE}"
+      >&2 echo -e "${P_INFO}The function '${P_END}generate_xml_schema${P_INFO}' must take two arguments:${P_END} <the metaschema file> <the location of the generated schema>${P_END}"
+      exit 2
+    fi
+
+    if ! function_exists "generate_xml_schema"; then
+      >&2 echo -e "${P_ERROR}The function '${P_END}generate_xml_schema${P_ERROR}' isn't defined in:${P_END} ${PROVIDER_INIT_FILE}"
+      >&2 echo -e "${P_INFO}The function '${P_END}generate_xml_schema${P_INFO}' must take two arguments:${P_END} <the metaschema file> <the location of the generated schema>${P_END}"
+      exit 3
+    fi
+
+    if ! function_exists "generate_json_schema"; then
+      >&2 echo -e "${P_ERROR}The function '${P_END}generate_json_schema${P_ERROR}' isn't defined in:${P_END} ${PROVIDER_INIT_FILE}"
+      >&2 echo -e "${P_INFO}The function '${P_END}generate_json_schema${P_INFO}' must take two arguments:${P_END} <the metaschema file> <the location of the generated schema>${P_END}"
+      exit 4
+    fi
   }
 
   if [ -z ${METASCHEMA_SCRIPT_DIR+x} ]; then
@@ -72,26 +130,12 @@ if [ -z ${METASCHEMA_SCRIPT_INIT+x} ]; then
     source "${METASCHEMA_SCRIPT_DIR}/config.sh"
   fi
 
-  # set meaningful defaults if not set above
-  if [ -z ${PROVIDER_DIR+x} ]; then
-    PROVIDER_DIR="${METASCHEMA_SCRIPT_DIR}/../toolchains/oscal-m2"
+  initialize_defaults
+  init_provider
+  cmd_exitcode=$?
+  if [ $cmd_exitcode -ne 0 ]; then
+    >&2 echo -e "${P_ERROR}Failed to initialize the Metaschema provider '${P_END}${PROVIDER_DIR}${P_ERROR}'.${P_END}"
   fi
-
-  if [ -z ${WORKING_DIR+x} ]; then
-    WORKING_DIR="${PWD}"
-  fi
-    
-  if [ -z ${CACHE_DIR+x} ]; then
-    CACHE_DIR="${WORKING_DIR}/.metaschema-cache"
-  fi
-
-  if [ -z ${VERBOSE+x} ]; then
-    VERBOSE=true
-  fi
-
-  PROVIDER_DIR=$(get_abs_path "${PROVIDER_DIR}")
-  WORKING_DIR=$(get_abs_path "${WORKING_DIR}")
-  CACHE_DIR=$(get_abs_path "${CACHE_DIR}")
 
   # Make directories
   mkdir -p "${WORKING_DIR}"
@@ -101,13 +145,13 @@ if [ -z ${METASCHEMA_SCRIPT_INIT+x} ]; then
   # check if stdout is a terminal...
   export TERM=${TERM:-dumb}
   colorize=0
-  if [ -t 1 ]; then
+#  if [ -t 1 ]; then
     # does the terminal support colors?
     ncolors=$(tput colors)
     if [ -n "$ncolors" ] && [ $ncolors -ge 8 ]; then
       colorize=1
     fi
-  fi
+#  fi
 
   if [ $colorize -eq 1 ]; then
       #setup print colors
