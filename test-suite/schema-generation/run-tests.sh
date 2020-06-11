2767badbd07a210ec42791827bb04eec6b9b7d0b
Wendell Piez <wendell.piez@nist.gov>
2020-06-11 13:06:30 -0400

    Reverted run-tests.sh to repo version

--- test-suite/schema-generation/run-tests.sh
+++ test-suite/schema-generation/run-tests.sh
@@ -10,8 +10,7 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)/../../scripts/i
 
 # configuration
 UNIT_TESTS_DIR=$(get_abs_path "${METASCHEMA_SCRIPT_DIR}/../test-suite/schema-generation")
-METASCHEMA_SCHEMA=$(get_abs_path "${METASCHEMA_SCRIPT_DIR}/../toolchains/xslt-proto-v04/validate")
-
+METASCHEMA_SCHEMA=$(get_abs_path "${METASCHEMA_SCRIPT_DIR}/../toolchains/oscal-m2/lib/metaschema.xsd")
 DEBUG="false"
 
 # Option defaults
@@ -209,7 +208,7 @@ if [ "$VERBOSE" = "true" ]; then
 fi
 
 # compile the schematron
-metaschema_lib=$(get_abs_path "${METASCHEMA_SCRIPT_DIR}/../toolchains/xslt-proto-v04/validate")
+metaschema_lib=$(get_abs_path "${METASCHEMA_SCRIPT_DIR}/../toolchains/oscal-m2/lib")
 schematron="$metaschema_lib/metaschema-check.sch"
 compiled_schematron="${SCRATCH_DIR}/metaschema-schematron-compiled.xsl"
 
